d65d33b9a6dd3a5b513cc598abe6a51aa4b86dc0
gwarz <gmwarzecha@tutanota.com>
2021-07-14 07:28:26 -0700

    added a new component that takes in children as props and makes the toggle fully reusable

--- src/App.js
+++ src/App.js
@@ -1,29 +1,21 @@
 import logo from "./logo.svg";
 import "./App.css";
-import Toggle from "./ToggleRenderProps";
+import Toggle from "./ToggleRPC";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <Toggle
-          render={({ on, toggle }) => (
-            <div>
-              {on && <nav>NavItem</nav>}
-              <button onClick={toggle}>Show / Hide</button>
-            </div>
-          )}
-        />
-        <Toggle
-          render={({ on, toggle }) => (
-            <div>
-              {on && <nav>NavItem</nav>}
-              <button onClick={toggle}>Menu</button>
-            </div>
-          )}
-        />
       </header>
+      <Toggle>
+        {({ on, toggle }) => (
+          <div>
+            {on && <h1>Show Me</h1>}
+            <button onClick={toggle}>Show / Hide</button>
+          </div>
+        )}
+      </Toggle>
     </div>
   );
 }
