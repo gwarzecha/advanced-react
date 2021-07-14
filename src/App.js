eac24ae8e62c1169a11dcd67db738a8c9560d1d2
gwarz <gmwarzecha@tutanota.com>
2021-07-14 07:12:10 -0700

    set up files via CRA and added a couple of toggle components, built basic toggle

--- src/App.js
+++ src/App.js
@@ -1,22 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Toggle from "./ToggleRenderProps";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Toggle
+          render={({ on, toggle }) => (
+            <div>
+              {on && <nav>NavItem</nav>}
+              <button onClick={toggle}>Show / Hide</button>
+            </div>
+          )}
+        />
+        <Toggle
+          render={({ on, toggle }) => (
+            <div>
+              {on && <nav>NavItem</nav>}
+              <button onClick={toggle}>Menu</button>
+            </div>
+          )}
+        />
       </header>
     </div>
   );
